7730241a98b524c381ed2385b53a19bd44d9d8e5
Francisco <francisco.alles@gmail.com>
2021-05-26 22:02:34 -0300

    commit

--- bucket.tf
+++ bucket.tf
@@ -2,7 +2,7 @@ resource "oci_objectstorage_bucket" "test_bucket" {
     #Required
     compartment_id = var.compartment_ocid
     name = "Bucket"
-    namespace = "Bucket"
+    namespace = "ax3jtjrhgkjy"
 
    
 }
