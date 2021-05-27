a3f34c9973b943137fd03a7a4a488b5b59d9b7ce
Francisco <francisco.alles@gmail.com>
2021-05-26 22:10:21 -0300

    commit

--- datasources.tf
+++ datasources.tf
@@ -31,9 +31,3 @@ data "oci_core_images" "lab_images"{
     operating_system = "Oracle Linux"
 
 }
-
-
-data "oci_database_autonomous_database" "test_autonomous_database" {
-    #Required
-    autonomous_database_id = oci_database_autonomous_database.test_autonomous_database.id
-}
