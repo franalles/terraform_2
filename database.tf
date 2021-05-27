f349bed938deac8ebea098e0649f443eb17b242c
Francisco <francisco.alles@gmail.com>
2021-05-26 21:27:22 -0300

    commit

--- database.tf
+++ database.tf
@@ -1,7 +1,7 @@
 resource "oci_database_autonomous_database" "test_autonomous_database" {
     #Required
     compartment_ocid = var.compartment_ocid
-    cpu_core_count = "2"
+    cpu_core_count = "1"
     db_name = "Test Database"
 
     #Optional
